c9293d9da78d0d7d14eb1ba9a9ac85cafb3ef639
Promise Feliti <promisefeli@protonmail.com>
2021-08-31 21:38:05 +0200

    Add project files.

---
+++ WebCalculator/wwwroot/js/JavaScript.js
@@ -0,0 +1,72 @@
+﻿//simple Calculator
+
+var e = document.getElementById("opList");
+var firstNum;
+var secondNum;
+var opp;
+
+function handleClick() {
+    firstNum = parseInt(simpleCalc.num1.value);
+    opp = simpleCalc.oparator.value;
+    secondNum = parseInt(simpleCalc.num2.value);
+    var result;
+    switch (opp) {
+        case "+":
+            result = firstNum + secondNum;
+            break;
+        case "-":
+            result = firstNum - secondNum;
+            break;
+        case "/":
+            result = firstNum / secondNum;
+            break;
+        case "*":
+            result = firstNum * secondNum;
+            break;
+        case "%":
+            result = firstNum % secondNum;
+            break;
+        default:
+            result = "error"
+    }
+    simpleCalc.display.value = result;
+    document.getElementById("display").change();
+}
+
+
+function calculate(num1,operator, num2) {
+    var result = 0;
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "/":
+            result = num1 / num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "%":
+            result = num1 % num2;
+            break;
+        default:
+            result = "error"
+    }
+    return result + "";
+}
+
+    function setValue() {
+        console.log("Changing options...");
+        selectValue = document.getElementById("opList").value;
+        console.log("selectValue :" + selectValue);
+    }
+
+//function init() {
+//    var button = document.getElementById("button");
+//    button.onclick = handleClick;
+//}
+
+//window.onload = init;
---
+++ WebCalculatorTests/SimpleCalculatorTests.cs
@@ -0,0 +1,66 @@
+﻿
+using WebCalculator.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.JSInterop;
+
+namespace WebCalculator.Pages.Tests
+{
+    [TestClass()]
+    public class SimpleCalculatorTests
+    {
+       
+        
+        [TestMethod()]
+        public void EvaluateAsyncTestAdd()
+        {
+            
+            SimpleCalculator calc = new SimpleCalculator();
+            calc.Num1 = "1";
+            calc.Num2 = "2";
+            calc.Opp = "+";
+            //calc.EvaluateAsync(num1: calc.Num1, opp: calc.Opp, num2: calc.Num2);
+            Assert.AreEqual<string>(expected: "3", "3");
+        }
+
+        [TestMethod()]
+        public void EvaluateAsyncTestMinus()
+        {
+
+            SimpleCalculator calc = new SimpleCalculator();
+            calc.Num1 = "4";
+            calc.Num2 = "2";
+            calc.Opp = "-";
+            //calc.EvaluateAsync(num1: calc.Num1, opp: calc.Opp, num2: calc.Num2);
+            Assert.AreEqual<string>(expected: "2", "2");
+        }
+
+        [TestMethod()]
+        public void EvaluateAsyncTestMultiply()
+        {
+
+            SimpleCalculator calc = new SimpleCalculator();
+            calc.Num1 = "4";
+            calc.Num2 = "5";
+            calc.Opp = "+";
+            //calc.EvaluateAsync(num1: calc.Num1, opp: calc.Opp, num2: calc.Num2);
+            Assert.AreEqual<string>(expected: "20", "20");
+        }
+        [TestMethod()]
+        public void EvaluateAsyncTestDIvide()
+        {
+
+            SimpleCalculator calc = new SimpleCalculator();
+            calc.Num1 = "9";
+            calc.Num2 = "3";
+            calc.Opp = "+";
+            //calc.EvaluateAsync(num1: calc.Num1, opp: calc.Opp, num2: calc.Num2);
+            Assert.AreEqual<string>(expected: "3", "3");
+        }
+    }
+}
